7ff86e5162edb01b321902fe5e2505741270c276
juracal <jrcdic16@gmail.com>
2022-08-25 19:00:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute(" use role accountadmin; select * from fruit_load_list;")
+my_cur.execute("select current_database()")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load contains: ")
 streamlit.dataframe(my_data_rows)
